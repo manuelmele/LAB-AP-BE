fd34b0ae594b4605cae3e415296fba9b7eebf6c8
parwal <parwal.lp@gmail.com>
2022-02-13 11:00:11 +0100

    update RegisterRequest
    ho aggiornato i messaggi di errore relativi alla signUp, in modo da essere compatibili con il frontend

--- src/main/java/core/wefix/lab/utils/object/request/RegisterRequest.java
+++ src/main/java/core/wefix/lab/utils/object/request/RegisterRequest.java
@@ -21,37 +21,32 @@ public class RegisterRequest {
 	public static String validateRegisterRequestJsonFields(RegisterRequest registerRequest) {
 
 		if (!registerRequest.getFirstName().matches(firstNameRegex) || registerRequest.getFirstName() == null) {
-			String validationError = "Json field \"firstName\" : " + registerRequest.getFirstName() + " is invalid";
+			String validationError = "Json field \"firstName\" is invalid";
 			System.out.println(validationError);
 			return validationError;
 		}
 
 		if (!registerRequest.getSecondName().matches(secondNameRegex) || registerRequest.getSecondName() == null) {
-			String validationError = "Json field \"secondName\" : " + registerRequest.getSecondName() + " is invalid";
+			String validationError = "Json field \"surname\" is invalid";
 			System.out.println(validationError);
 			return validationError;
 		}
 
 		if (!registerRequest.getEmail().matches(emailRegex) || registerRequest.getEmail() == null) {
-			String validationError = "Json field \"email\" : " + registerRequest.getEmail() + " is invalid";
+			String validationError = "Json field \"email\" is invalid";
 			System.out.println(validationError);
 			return validationError;
 		}
 
 		if (!registerRequest.getUserPassword().matches(passwordRegex) || registerRequest.getUserPassword() == null) {
-			String validationError = "Json field \"password\" : " + registerRequest.getUserPassword() + " is invalid";
+			String validationError = "Json field \"password\" is invalid";
 			System.out.println(validationError);
 			return validationError;
 		}
 
-		if (!registerRequest.getUserConfirmPassword().matches(passwordRegex) || registerRequest.getUserConfirmPassword() == null) {
-			String validationError = "Json field \"confirmPassword\" : " + registerRequest.getUserConfirmPassword() + " is invalid";
-			System.out.println(validationError);
-			return validationError;
-		}
 
 		if(!registerRequest.getUserConfirmPassword().equals(registerRequest.getUserPassword())) {
-			String validationError = "Json field \"password and confirmPassword\" : " + registerRequest.getUserPassword() + " and " + registerRequest.getUserConfirmPassword() + " not match";
+			String validationError = "Json field \"password and confirmPassword\" don't match";
 			System.out.println(validationError);
 			return validationError;
 		}
